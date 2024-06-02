09bac51b7429ec20b387a16db2f4881f91980f76
Quang Nguyen <dangquangdev2k@gmail.com>
2024-06-03 05:29:30 +0700

    update css animation in carousel and header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MovieManagementPractice from "./pages/movie-management";
 import HomePage from "./pages/home";
 import Layout from "./components/layout";
+import Login from "./pages/login";
 
 function App() {
   const router = createBrowserRouter([
@@ -21,8 +22,13 @@ function App() {
           path: "/home",
           element: <HomePage />,
         },
+        // {
+        //   path: "/login",
+        //   element: <Login />,
+        // },
       ],
     },
+    { path: "/login", element: <Login /> },
   ]);
   return (
     <div>
